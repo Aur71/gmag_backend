1b0e75aac2d98d610b431f021cf34e2a0e98aedc
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-04-21 09:23:33 +0300

    product model stuff added

--- src/api/v1/models/Product.js
+++ src/api/v1/models/Product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const ImageSchema = require('./schemas/Image').schema;
 
 const ProductSchema = new mongoose.Schema({
   type: {
@@ -13,6 +14,43 @@ const ProductSchema = new mongoose.Schema({
     type: String,
     default: 'not featured',
   },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  currentPrice: {
+    type: Number,
+    required: true,
+  },
+  oldPrice: {
+    type: Number,
+    default: 0,
+  },
+  discount: {
+    type: Number,
+    default: 0,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  reviewsCount: {
+    type: Number,
+    default: 0,
+  },
+  stock: {
+    type: Number,
+    default: 0,
+  },
+  orders: {
+    type: Number,
+    default: 0,
+  },
+  date: {
+    type: Number,
+    default: Date.now,
+  },
+  images: [ImageSchema],
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
---
+++ src/api/v1/models/schemas/Image.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const ImageSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports.schema = ImageSchema;
