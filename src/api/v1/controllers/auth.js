af922e26f4104131ecd66025a8c67be40d14388e
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-04-25 15:43:59 +0300

    login added

--- src/api/v1/controllers/auth.js
+++ src/api/v1/controllers/auth.js
@@ -1,17 +1,30 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
+};
 
 const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).send({ email, user });
+    const token = createToken(user._id);
+    res.status(200).send({ email, token });
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
 };
 
 const loginUser = async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).send({ email, token });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 };
 
 const logoutUser = (req, res) => {
--- src/api/v1/models/User.js
+++ src/api/v1/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema(
   {
@@ -11,19 +12,34 @@ const userSchema = new mongoose.Schema(
     },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    profileImage: { type: String, default: null },
-    // add a default user image from firebase
+    profileImage: {
+      type: String,
+      default:
+        'https://firebasestorage.googleapis.com/v0/b/gmag-b6a34.appspot.com/o/users%2Fuser.png?alt=media&token=701f682d-d622-412a-8788-1cd46d5c86d7',
+    },
   },
   { timestamps: true }
 );
 
-// static methods
 userSchema.statics.signup = async function (email, password) {
+  if (!email || !password) throw Error('All fields must be filled');
+  if (!validator.isEmail(email)) throw Error('Email is not valid');
+  if (!validator.isStrongPassword(password))
+    throw Error('Password not strong enough');
   const exists = await this.findOne({ email });
   if (exists) throw Error('Email already in use');
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
   const user = await this.create({ email, password: hash });
+  return user;
+};
+
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) throw Error('All fields must be filled');
+  const user = await this.findOne({ email });
+  if (!user) throw Error('Incorrect email');
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) throw Error('Incorrect password');
   return user;
 };
 
