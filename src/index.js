d6b9c36bf3699968e976de9b07b09dc5fb6ec90a
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-04-26 13:23:14 +0300

    cors added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const auth = require('./api/v1/routes/auth');
 const promotionSlider = require('./api/v1/routes/promotionSlider');
 const products = require('./api/v1/routes/products');
@@ -10,6 +11,11 @@ require('dotenv').config();
 
 // middleware
 app.use(express.json());
+app.use(
+  cors({
+    origin: 'http://localhost:3001',
+  })
+);
 
 // routes
 app.use('/api/v1/auth', auth);
