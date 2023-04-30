3a5d6479979b1dbf69a1c2d06011439b068a1af2
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-04-30 17:18:19 +0300

    added timestamps to questions and answers

--- src/api/v1/models/schemas/questions/Answer.js
+++ src/api/v1/models/schemas/questions/Answer.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 
-const AnswerSchema = new mongoose.Schema({
-  postedOn: {
-    type: Number,
-    default: Date.now,
+const AnswerSchema = new mongoose.Schema(
+  {
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    answer: {
+      type: String,
+      required: true,
+    },
   },
-  postedBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  answer: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports.schema = AnswerSchema;
--- src/api/v1/models/schemas/questions/Question.js
+++ src/api/v1/models/schemas/questions/Question.js
@@ -1,21 +1,20 @@
 const mongoose = require('mongoose');
 const Answer = require('./Answer').schema;
 
-const QuestionSchema = new mongoose.Schema({
-  postedOn: {
-    type: Number,
-    default: Date.now,
+const QuestionSchema = new mongoose.Schema(
+  {
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    question: {
+      type: String,
+      required: true,
+    },
+    answers: [Answer],
   },
-  postedBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  question: {
-    type: String,
-    required: true,
-  },
-  answers: [Answer],
-});
+  { timestamps: true }
+);
 
 module.exports.schema = QuestionSchema;
