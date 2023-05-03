4b1b73ed554a7b2584f500d18b44bb9552efd53c
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-05-03 16:32:09 +0300

    Update reviews route

--- src/api/v1/controllers/reviews.js
+++ src/api/v1/controllers/reviews.js
@@ -24,8 +24,31 @@ const addReview = async (req, res) => {
   }
 };
 
-const updateReview = (req, res) => {
-  res.send('update review');
+const updateReview = async (req, res) => {
+  const { productId, reviewId } = req.params;
+  const userId = req.user._id;
+  const { stars, title, content } = req.body;
+
+  try {
+    const product = await Product.findById(productId);
+    const review = product.reviews.id(reviewId);
+    if (!product) {
+      return res.status(404).send({ error: 'Product not found' });
+    }
+    if (!review) {
+      return res.status(404).json({ error: 'Review not found' });
+    }
+    if (review.postedBy.toString() !== userId.toString()) {
+      return res.status(401).json({ error: 'You are not authorized' });
+    }
+    review.stars = stars;
+    review.title = title;
+    review.content = content;
+    await product.save();
+    res.status(200).json('Review updated');
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
 };
 
 const deleteReview = (req, res) => {
