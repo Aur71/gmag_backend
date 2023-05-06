1d52938cad9c491e3a859d6f93b3894f00d0480b
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-05-06 12:39:14 +0300

    Edit question added

--- src/api/v1/controllers/questions.js
+++ src/api/v1/controllers/questions.js
@@ -23,12 +23,53 @@ const addQuestion = async (req, res) => {
   }
 };
 
-const updateQuestion = (req, res) => {
-  res.send('Update question');
+const updateQuestion = async (req, res) => {
+  const { newQuestion } = req.body;
+  const { productId, questionId } = req.params;
+  const userId = req.user._id;
+
+  try {
+    const product = await Product.findById(productId);
+    const question = product.questions.id(questionId);
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    if (!question) {
+      return res.status(404).json({ error: 'Question not found' });
+    }
+    if (question.postedBy.toString() !== userId.toString()) {
+      return res.status(401).json({ error: 'You are not authorized' });
+    }
+    question.question = newQuestion;
+    await product.save();
+    res.status(201).json('Question updated');
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
 };
 
-const deleteQuestion = (req, res) => {
-  res.send('Delete question');
+const deleteQuestion = async (req, res) => {
+  const { productId, questionId } = req.params;
+  const userId = req.user._id;
+
+  try {
+    const product = await Product.findById(productId);
+    const question = product.questions.id(questionId);
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    if (!question) {
+      return res.status(404).json({ error: 'Question not found' });
+    }
+    if (question.postedBy.toString() !== userId.toString()) {
+      return res.status(401).json({ error: 'You are not authorized' });
+    }
+    product.questions.pull({ _id: questionId });
+    await product.save();
+    res.status(200).json('Question deleted');
+  } catch (error) {
+    return res.status(404).json({ error: 'Question not found' });
+  }
 };
 
 const addAnswer = async (req, res) => {
