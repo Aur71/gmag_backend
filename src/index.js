59ef5ee8f6cbf2ba22642476b0d31911f3de3681
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-05-14 18:45:35 +0300

    redy for initial deployment

--- src/index.js
+++ src/index.js
@@ -35,7 +35,10 @@ const port = 3000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGODB_URI);
-    app.listen(port, console.log(`Server is listening on port: ${port}`));
+    app.listen(
+      process.env.PORT || port,
+      console.log(`Server is listening on port: ${port}`)
+    );
   } catch (error) {
     console.log(error);
   }
