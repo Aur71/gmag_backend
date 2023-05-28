66c41f74121cca007c554eee89392891e41ffb1f
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-05-28 10:56:30 +0300

    favorites

--- src/api/v1/controllers/favorites.js
+++ src/api/v1/controllers/favorites.js
@@ -24,18 +24,38 @@ const addList = async (req, res) => {
     user.favorites.lists.push(newList);
     if (useAsMainList) user.favorites.mainList = name;
     await user.save();
-    return res.send(user.favorites);
+    res.status(201).send(user.favorites);
   } catch (error) {
     res.status(500).json({ error: 'Server error' });
   }
 };
 
-const deleteList = (req, res) => {
-  res.send('delete list');
+const editList = async (req, res) => {
+  const userId = req.user._id;
+  const { listId } = req.params;
+  const { name, useAsMainList } = req.body;
+  if (!userId) res.status(404).json({ error: 'User not found' });
+
+  try {
+    const user = await User.findById(userId).select('favorites');
+    if (!user) return res.status(404).json({ error: 'User not found' });
+    const list = user.favorites.lists.find(
+      (list) => list._id.toString() === listId.toString()
+    );
+    if (!list) return res.status(404).json({ error: 'List not found' });
+    if (user.favorites.mainList === list.name && !useAsMainList)
+      user.favorites.mainList = user.favorites.lists[1].name;
+    if (useAsMainList) user.favorites.mainList = name;
+    list.name = name;
+    await user.save();
+    res.status(201).send(user.favorites);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
 };
 
-const editList = (req, res) => {
-  res.send('edit list');
+const deleteList = async (req, res) => {
+  res.send('delete list');
 };
 
 const addProductToFavorites = (req, res) => {
