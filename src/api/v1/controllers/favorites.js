4e3c379e255332f5c41ec86f8cdb1b78d1619d15
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-05-29 12:08:57 +0300

    add products to favorites & populate products

--- src/api/v1/controllers/favorites.js
+++ src/api/v1/controllers/favorites.js
@@ -4,9 +4,13 @@ const getFavorites = async (req, res) => {
   const userId = req.user._id;
   if (!userId) return res.status(404).json({ error: 'User not found' });
   try {
-    const user = await User.findById(userId).select('favorites');
+    const user = await User.findById(userId).populate({
+      path: 'favorites.lists.products',
+      select:
+        '_id type name thumbnail currentPrice oldPrice rating reviewsCount',
+    });
     if (!user) return res.status(404).json({ error: 'User not found' });
-    res.status(201).send(user);
+    res.status(201).send(user.favorites);
   } catch (error) {
     res.status(500).json({ error: 'Server error' });
   }
@@ -16,9 +20,12 @@ const addList = async (req, res) => {
   const userId = req.user._id;
   const { name, useAsMainList } = req.body;
   if (!userId) res.status(404).json({ error: 'User not found' });
-
   try {
-    const user = await User.findById(userId).select('favorites');
+    const user = await User.findById(userId).populate({
+      path: 'favorites.lists.products',
+      select:
+        '_id type name thumbnail currentPrice oldPrice rating reviewsCount',
+    });
     if (!user) return res.status(404).json({ error: 'User not found' });
     const newList = { name, products: [] };
     user.favorites.lists.push(newList);
@@ -35,9 +42,12 @@ const editList = async (req, res) => {
   const { listId } = req.params;
   const { name, useAsMainList } = req.body;
   if (!userId) res.status(404).json({ error: 'User not found' });
-
   try {
-    const user = await User.findById(userId).select('favorites');
+    const user = await User.findById(userId).populate({
+      path: 'favorites.lists.products',
+      select:
+        '_id type name thumbnail currentPrice oldPrice rating reviewsCount',
+    });
     if (!user) return res.status(404).json({ error: 'User not found' });
     const list = user.favorites.lists.find(
       (list) => list._id.toString() === listId.toString()
@@ -58,9 +68,12 @@ const deleteList = async (req, res) => {
   const userId = req.user._id;
   const { listId } = req.params;
   if (!userId) res.status(404).json({ error: 'User not found' });
-
   try {
-    const user = await User.findById(userId).select('favorites');
+    const user = await User.findById(userId).populate({
+      path: 'favorites.lists.products',
+      select:
+        '_id type name thumbnail currentPrice oldPrice rating reviewsCount',
+    });
     if (!user) return res.status(404).json({ error: 'User not found' });
     const list = user.favorites.lists.find(
       (list) => list._id.toString() === listId.toString()
@@ -76,8 +89,33 @@ const deleteList = async (req, res) => {
   }
 };
 
-const addProductToFavorites = (req, res) => {
-  res.send('add product to list');
+const addProductToFavorites = async (req, res) => {
+  const userId = req.user._id;
+  const { _id } = req.body;
+  try {
+    const user = await User.findById(userId).select('favorites');
+    if (!user) return res.status(404).json({ error: 'User not found' });
+    const mainList = user.favorites.lists.find(
+      (list) => list.name === user.favorites.mainList
+    );
+    if (!mainList) return res.status(404).json({ error: 'List not found' });
+    if (!_id) return res.status(404).json({ error: 'Product not found' });
+    const isProductAlreadyInMainList = mainList.products.some(
+      (id) => id.toString() === _id.toString()
+    );
+    if (isProductAlreadyInMainList)
+      return res.status(409).json({ error: 'Product already in favorites' });
+    mainList.products.push(_id);
+    const allProducts = user.favorites.lists[0];
+    const isProductAlreadyInAllProductsList = allProducts.products.some(
+      (id) => id.toString() === _id.toString()
+    );
+    if (!isProductAlreadyInAllProductsList) allProducts.products.push(_id);
+    await user.save();
+    res.status(201).send('Product added to favorites');
+  } catch (error) {
+    res.status(500).send({ error: 'Server error' });
+  }
 };
 
 const removeProductFromFavorites = (req, res) => {
